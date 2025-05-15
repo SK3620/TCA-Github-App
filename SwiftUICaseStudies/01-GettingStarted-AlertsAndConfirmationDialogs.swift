42c329ee1a9ca3a766e6a15953c4a1ecb9275d4e
SK3620 <k.n.t11193620@gmail.com>
2025-05-15 10:34:47 +0900

    update: @Presents, AlertState, ConfirmationDialogState, PresentationState, .iflet 解説コメントアウト追記

--- SwiftUICaseStudies/01-GettingStarted-AlertsAndConfirmationDialogs.swift
+++ SwiftUICaseStudies/01-GettingStarted-AlertsAndConfirmationDialogs.swift
@@ -18,106 +18,126 @@ private let readMe = """
 
 @Reducer
 struct AlertAndConfirmationDialog {
-  @ObservableState
-  struct State: Equatable {
-    @Presents var alert: AlertState<Action.Alert>?
-    @Presents var confirmationDialog: ConfirmationDialogState<Action.ConfirmationDialog>?
-    var count = 0
-  }
-
-  enum Action {
-    case alert(PresentationAction<Alert>)
-    case alertButtonTapped
-    case confirmationDialog(PresentationAction<ConfirmationDialog>)
-    case confirmationDialogButtonTapped
-
-    @CasePathable
-    enum Alert {
-      case incrementButtonTapped
+    @ObservableState
+    struct State: Equatable {
+        
+        /*
+         @Presents
+         アラート表示するかどうか（＝nil か そうでないか）を監視しつつ、関連アクションも自動的に処理できるようにするマーク
+         
+         AlertState<Action>
+         ユーザーに表示されるアラートの状態を表す
+         Actionジェネリック: アラート内のボタンをタップすることで送信できるアクションの種類
+         */
+        
+        @Presents var alert: AlertState<Action.Alert>?
+        @Presents var confirmationDialog: ConfirmationDialogState<Action.ConfirmationDialog>?
+        var count = 0
     }
-    @CasePathable
-    enum ConfirmationDialog {
-      case incrementButtonTapped
-      case decrementButtonTapped
-    }
-  }
-
-  var body: some Reducer<State, Action> {
-    Reduce { state, action in
-      switch action {
-      case .alert(.presented(.incrementButtonTapped)),
-        .confirmationDialog(.presented(.incrementButtonTapped)):
-        state.alert = AlertState { TextState("Incremented!") }
-        state.count += 1
-        return .none
-
-      case .alert:
-        return .none
-
-      case .alertButtonTapped:
-        state.alert = AlertState {
-          TextState("Alert!")
-        } actions: {
-          ButtonState(role: .cancel) {
-            TextState("Cancel")
-          }
-          ButtonState(action: .incrementButtonTapped) {
-            TextState("Increment")
-          }
-        } message: {
-          TextState("This is an alert")
+    
+    enum Action {
+        
+        /*
+         PresentationAction
+         アラートやダイアログでユーザーがボタンを押したことを、Reducer に伝える
+         */
+        
+        case alert(PresentationAction<Alert>)
+        case alertButtonTapped
+        case confirmationDialog(PresentationAction<ConfirmationDialog>)
+        case confirmationDialogButtonTapped
+        
+        @CasePathable
+        enum Alert {
+            case incrementButtonTapped
         }
-        return .none
-
-      case .confirmationDialog(.presented(.decrementButtonTapped)):
-        state.alert = AlertState { TextState("Decremented!") }
-        state.count -= 1
-        return .none
-
-      case .confirmationDialog:
-        return .none
-
-      case .confirmationDialogButtonTapped:
-        state.confirmationDialog = ConfirmationDialogState {
-          TextState("Confirmation dialog")
-        } actions: {
-          ButtonState(role: .cancel) {
-            TextState("Cancel")
-          }
-          ButtonState(action: .incrementButtonTapped) {
-            TextState("Increment")
-          }
-          ButtonState(action: .decrementButtonTapped) {
-            TextState("Decrement")
-          }
-        } message: {
-          TextState("This is a confirmation dialog.")
+        @CasePathable
+        enum ConfirmationDialog {
+            case incrementButtonTapped
+            case decrementButtonTapped
         }
-        return .none
-      }
     }
-    .ifLet(\.$alert, action: \.alert)
-    .ifLet(\.$confirmationDialog, action: \.confirmationDialog)
-  }
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .alert(.presented(.incrementButtonTapped)),
+                    .confirmationDialog(.presented(.incrementButtonTapped)):
+                state.alert = AlertState { TextState("Incremented!") }
+                state.count += 1
+                return .none
+                
+            case .alert:
+                return .none
+                
+            case .alertButtonTapped:
+                // AlertState をセットし、アラート表示
+                state.alert = AlertState {
+                    TextState("Alert!")
+                } actions: {
+                    ButtonState(role: .cancel) {
+                        TextState("Cancel")
+                    }
+                    ButtonState(action: .incrementButtonTapped) {
+                        TextState("Increment")
+                    }
+                } message: {
+                    TextState("This is an alert")
+                }
+                return .none
+                
+            case .confirmationDialog(.presented(.decrementButtonTapped)):
+                state.alert = AlertState { TextState("Decremented!") }
+                state.count -= 1
+                return .none
+                
+            case .confirmationDialog:
+                return .none
+                
+            case .confirmationDialogButtonTapped:
+                state.confirmationDialog = ConfirmationDialogState {
+                    TextState("Confirmation dialog")
+                } actions: {
+                    ButtonState(role: .cancel) {
+                        TextState("Cancel")
+                    }
+                    ButtonState(action: .incrementButtonTapped) {
+                        TextState("Increment")
+                    }
+                    ButtonState(action: .decrementButtonTapped) {
+                        TextState("Decrement")
+                    }
+                } message: {
+                    TextState("This is a confirmation dialog.")
+                }
+                return .none
+            }
+        }
+        /*
+         .iflet 親の状態（State）にある @Presentsな子の状態（Optional） があるときだけ、その子のロジック（Reducer）を埋め込む
+         */
+        .ifLet(\.$alert, action: \.alert) // Reducer を返す
+        .ifLet(\.$confirmationDialog, action: \.confirmationDialog) // Reducer を返す
+    }
 }
 
 struct AlertAndConfirmationDialogView: View {
-  @Bindable var store: StoreOf<AlertAndConfirmationDialog>
-
-  var body: some View {
-    Form {
-      Section {
-        AboutView(readMe: readMe)
-      }
-
-      Text("Count: \(store.count)")
-      Button("Alert") { store.send(.alertButtonTapped) }
-      Button("Confirmation Dialog") { store.send(.confirmationDialogButtonTapped) }
+    @Bindable var store: StoreOf<AlertAndConfirmationDialog>
+    
+    var body: some View {
+        Form {
+            Section {
+                AboutView(readMe: readMe)
+            }
+            
+            Text("Count: \(store.count)")
+            Button("Alert") { store.send(.alertButtonTapped) }
+            Button("Confirmation Dialog") { store.send(.confirmationDialogButtonTapped) }
+        }
+        .navigationTitle("Alerts & Dialogs")
+        .alert($store.scope(state: \.alert, action: \.alert))
+        .confirmationDialog($store.scope(state: \.confirmationDialog, action: \.confirmationDialog))
     }
-    .navigationTitle("Alerts & Dialogs")
-    .alert($store.scope(state: \.alert, action: \.alert))
-    .confirmationDialog($store.scope(state: \.confirmationDialog, action: \.confirmationDialog))
-  }
 }
 
 #Preview {
