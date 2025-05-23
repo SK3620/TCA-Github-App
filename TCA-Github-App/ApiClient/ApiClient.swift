930b39169cf707ec3345bdf2c766e55dce9137f7
SK3620 <k.n.t11193620@gmail.com>
2025-05-23 23:54:57 +0900

    update: モックデータを返す

--- TCA-Github-App/ApiClient/ApiClient.swift
+++ TCA-Github-App/ApiClient/ApiClient.swift
@@ -8,26 +8,10 @@ public final class ApiClient: Sendable {
         self.session = session
     }
 
-    public func send<T: BaseRequest>(request: T) async throws -> T.Response {
-        do {
-            let response = try await session.response(for: request)
-            // let response: T.Response
-            return response
-        } catch let originalError as SessionTaskError {
-            switch originalError  {
-            case let .connectionError(error), let .responseError(error), let .requestError(error):
-                throw ApiError.unknown(error as NSError)
-            case let .responseError(error as ApiError):
-                throw error
-            }
-        }
-    }
-    
-    // モックデータを返す
-//    public func send<T: BaseRequest>(request: T) async throws -> T.Response where T.Response == SearchReposResponse {
+//    public func send<T: BaseRequest>(request: T) async throws -> T.Response {
 //        do {
-//            // とりあえず、mockデータを返す
-//            let response: T.Response = .mock()
+//            let response = try await session.response(for: request)
+//            // let response: T.Response
 //            return response
 //        } catch let originalError as SessionTaskError {
 //            switch originalError  {
@@ -38,6 +22,22 @@ public final class ApiClient: Sendable {
 //            }
 //        }
 //    }
+    
+    // モックデータを返す
+    public func send<T: BaseRequest>(request: T) async throws -> T.Response where T.Response == SearchReposResponse {
+        do {
+            // とりあえず、mockデータを返す
+            let response: T.Response = .mock()
+            return response
+        } catch let originalError as SessionTaskError {
+            switch originalError  {
+            case let .connectionError(error), let .responseError(error), let .requestError(error):
+                throw ApiError.unknown(error as NSError)
+            case let .responseError(error as ApiError):
+                throw error
+            }
+        }
+    }
 
     public static let liveValue = ApiClient(session: Session.shared)
     public static let testValue = ApiClient(session: Session(adapter: NoopSessionAdapter()))
